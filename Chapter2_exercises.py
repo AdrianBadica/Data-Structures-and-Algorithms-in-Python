507df3f22547b5343b79a66901c07087344c498a
AdrianBadica <badicaadrian1@yahoo.com>
2020-08-05 18:55:13 +0300

    Update Chapter2_exercises.py

--- Chapter2_exercises.py
+++ Chapter2_exercises.py
@@ -584,7 +584,27 @@ so that once a customer has made ten calls to charge in the current month,
 each additional call to that function results in an additional $1 surcharge.
 """
 
-
+class PredatoryCreditCard(CreditCard):
+    def __init__(self, customer,bank,acnt,limit,apr):
+        super().__init__(customer,bank,acnt,limit)
+        self._apr=apr
+    def charge(self,price, z=0,c=[]):
+        success=super().charge(price)
+        if not success:
+            self._balance+=5
+        c.append(1)
+        self._z=len(c)
+        return success
+    def process_month(self):
+        if self._z >= 10:
+            if self._balance > 0 :
+                monthly_factor = pow(1+self._apr,1/12)
+                self._balance+= (monthly_factor+self._z)
+        else:
+            if self._balance > 0 :
+                monthly_factor = pow(1+self._apr,1/12)
+                self._balance+= monthly_factor
+        print(self._balance, self._z)
  
                 
 
