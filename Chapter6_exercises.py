a44dfdeaceda4d07fc00026a008a00e9a457a472
AdrianBadica <badicaadrian1@yahoo.com>
2020-12-27 13:14:54 +0200

    Update Chapter6_exercises.py

--- Chapter6_exercises.py
+++ Chapter6_exercises.py
@@ -42,4 +42,28 @@ def rev_list(L):
     while not S.is_empty():
         L.append(S.pop())
     return L
+
+
+
+     
+
+"""R-6.13 Suppose you have a deque D containing the numbers (1,2,3,4,5,6,7,8),
+in this order. Suppose further that you have an initially empty queue Q.
+Give a code fragment that uses only D and Q (and no other variables) and
+results in D storing the elements in the order (1,2,3,5,4,6,7,8)."""
+
+Q.enqueue(D.delete_last()) # add 8 to queue
+Q.enqueue(D.delete_last()) # add 7 to queue
+Q.enqueue(D.delete_last()) # add 6 to queue
+
+
+D.add_first(D.delete_last()) # move 5 from the end of the deque to the start
+
+Q.enqueue(D.delete_last()) # add 4 to queue
+
+Q.enqueue(D.delete_first()) # add 5 to queue
+
+Q.enqueue(D.delete_last()) # add 3 to queue
+Q.enqueue(D.delete_last()) # add 2 to queue
+Q.enqueue(D.delete_last()) # add 1 to queue
         
